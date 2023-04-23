c6da1b60326cf46f8d8e22953dfa81eb63d8d064
Richard Osborn <richardosborn256@gmail.com>
2023-04-22 22:37:39 -0700

    Work

---
+++ HashParser/ListExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace HashParser
+{
+    /// <summary>
+    /// Helper methods.
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Count - 1.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int LastIndex(this IList col) => col.Count - 1;
+
+        /// <summary>
+        /// Fast removal from a list. Only use this if the order of items in <paramref name="src"/>
+        /// doesn't matter.
+        /// </summary>
+        public static void QuickRemove<T>(this List<T> src, T item)
+        {
+            int index = src.IndexOf(item);
+
+            if (index >= 0)
+                QuickRemove(src, index);
+        }
+
+        /// <summary>
+        /// Fast removal from a list. Only use this if the order of items in <paramref name="src"/>
+        /// doesn't matter.
+        /// </summary>
+        public static void QuickRemove<T>(this List<T> src, int index)
+        {
+            src.Swap(index, src.LastIndex());
+            src.RemoveLast();
+        }
+
+        /// <summary>
+        /// Swap element at index a with element at index b.
+        /// </summary>
+        public static void Swap<T>(this IList<T> list, int a, int b)
+        {
+            //perform swap
+            T tmp = list[a];
+            list[a] = list[b];
+            list[b] = tmp;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void RemoveFirst<T>(this List<T> list)
+            => list.RemoveAt(0);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void RemoveLast<T>(this List<T> list)
+            => list.RemoveAt(list.Count - 1);
+    }
+}
--- HashParser/Sorter.cs
+++ HashParser/Sorter.cs
@@ -13,7 +13,9 @@ namespace HashParser
 
         private readonly IReadOnlyCollection<ClothingInfo> allClothing;
         private readonly Dictionary<string, Dictionary<string, ClothingInfo[][]>> clothes_list = new Dictionary<string, Dictionary<string, ClothingInfo[][]>>();
-                
+
+        Dictionary<string, List<ClothingInfo>> table = new Dictionary<string, List<ClothingInfo>>();
+
         #region Constructors
 
         public Sorter(List<ClothingInfo> clothingInfos)
@@ -46,24 +48,49 @@ namespace HashParser
         {
             fixer.Fix(allClothing);
         }
-
-        private List<List<ClothingInfo>> Categorize(List<ClothingInfo> clothing)
+        
+        private List<List<List<ClothingInfo>>> Categorize(List<ClothingInfo> clothing)
         {
-            var variationCollection = new List<List<ClothingInfo>>();
+            var categories = new List<List<List<ClothingInfo>>>(8);
 
+            while (clothing.Count > 0)
+                categories.Add(CategorizeOnce(clothing));
+
+            return categories;
+        }
+
+        private List<List<ClothingInfo>> CategorizeOnce(List<ClothingInfo> clothing)
+        {
             string category = clothing[0].category_hashname;
 
             var categoryItems = clothing
-                .Where(item => item.category_hashname == category)
-                .ToList();
+                .Where(item => item.category_hashname == category);
 
+            foreach (var item in categoryItems)
+            {
+                (string Model, string Variation) = SplitHashTokens(item.hashname);
+                Categorize(Model, item);
+                clothing.QuickRemove(item);
+            }
 
-
+            var variationCollection = table.Values.ToList();
+            table.Clear();
 
             return variationCollection;
         }
 
-        private (string Model, string Variation) SplitHashTokens(string hashName)
+        private void Categorize(string Model, ClothingInfo clothingInfo)
+        {
+            if (!table.TryGetValue(Model, out List<ClothingInfo> variations))
+            {
+                variations = new List<ClothingInfo>(4);
+                table.Add(Model, variations);
+            }
+
+            variations.Add(clothingInfo);
+        }
+
+        private static (string Model, string Variation) SplitHashTokens(string hashName)
         {
             int i = 0;
             int start = 0;
